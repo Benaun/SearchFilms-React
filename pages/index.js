0e52334ef802b1b0b1c8740463733c98d2fc52a3
A_benaun <Andrey.kuhtin126@gmail.com>
2023-08-28 19:34:52 +0300

    Fix bugs

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,7 @@
 import FetchFilms from '../components/FetchFilms';
-import { useState } from 'react';
 
 export default function Home() {
-  const [value, setValue] = useState("");
-
-  const handleSearchInput = (e) => {
-    setValue(e.target.value);
-  };
-
   return <>
-    <FetchFilms search={value} />
+    <FetchFilms/>
   </>;
 }
